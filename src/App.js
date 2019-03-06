d13da9ef434c1e899ed146196809af968603efed
OttaviaFeletig <ottafel@hotmail.it>
2019-03-06 17:19:57 +0100

    state in App.js and passing name and img with props

--- src/App.js
+++ src/App.js
@@ -4,11 +4,54 @@ import Mentors from './components/Mentors'
 
 import './App.css';
 
+import alex from './mentors_pic/mentor-alex.jpg';
+import luis from './mentors_pic/mentor-luis.jpg';
+import mari from './mentors_pic/mentor-mari.jpg';
+import michal from './mentors_pic/mentor-michal.jpg';
+import pol from './mentors_pic/mentor-pol.jpg';
+import raul from './mentors_pic/mentor-raul.jpg';
+import ronni from './mentors_pic/mentor-ronni.jpg';
+import vasil from './mentors_pic/mentor-vasil.jpg';
+
 class App extends Component {
+  state = {
+    mentors: [{
+      name: 'Alex',
+      imgPath: alex
+    },
+    {
+      name: 'Luis',
+      imgPath: luis
+    },
+    {
+      name: 'Mari',
+      imgPath: mari
+    },
+    {
+      name: 'Michal',
+      imgPath: michal
+    },
+    {
+      name: 'Pol',
+      imgPath: pol
+    },
+    {
+      name: 'Raul',
+      imgPath: raul
+    },
+    {
+      name: 'Ronni',
+      imgPath: ronni
+    },
+    {
+      name: 'Vasil',
+      imgPath: vasil
+    }]
+  }
   render() {
     return (
       <div className="App">
-        <Mentors />
+        <Mentors mentors={this.state.mentors} />
       </div>
     );
   }
--- src/components/Mentors.js
+++ src/components/Mentors.js
@@ -4,46 +4,15 @@ import Mentor from './Mentor';
 
 import '../style/Mentors.css';
 
-import alex from '../mentors_pic/mentor-alex.jpg';
-import luis from '../mentors_pic/mentor-luis.jpg';
-import mari from '../mentors_pic/mentor-mari.jpg';
-import michal from '../mentors_pic/mentor-michal.jpg';
-import pol from '../mentors_pic/mentor-pol.jpg';
-import raul from '../mentors_pic/mentor-raul.jpg';
-import ronni from '../mentors_pic/mentor-ronni.jpg';
-import vasil from '../mentors_pic/mentor-vasil.jpg';
-
 export default class Mentors extends Component {
+
   render() {
     return (
-      <React.Fragment>
+      this.props.mentors.map(mentor => (
         <div className="mentor">
-          <Mentor name='Alex' imgPath={alex} />
+          <Mentor name={mentor.name} imgPath={mentor.imgPath} />
         </div>
-        <div className="mentor">
-          <Mentor name='Luis' imgPath={luis} />
-        </div>
-        <div className="mentor">
-          <Mentor name='Mari' imgPath={mari} />
-        </div>
-        <div className="mentor">
-          <Mentor name='Michal' imgPath={michal} />
-        </div>
-        <div className="mentor">
-          <Mentor name='Pol' imgPath={pol} />
-        </div>
-        <div className="mentor">
-          <Mentor name='Raul' imgPath={raul} />
-        </div>
-        <div className="mentor">
-          <Mentor name='Ronni' imgPath={ronni} />
-        </div>
-        <div className="mentor">
-          <Mentor name='Vasil' imgPath={vasil} />
-        </div>
-      </React.Fragment>
-      
-      
+      ))
     )
   }
 }
